9eacefaee387de0b460e8344e061e5da084e9cad
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-08-09 12:59:05 +0300

    uppercase() func: first attempt

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    upper_str = ""
+    for i in range(len(str)):
+        decimal = ord(str[i])
+        if (decimal >= 97 and decimal <= 122):
+            upper_str = upper_str + chr(decimal - 32)
+        else:
+            upper_str = upper_str + str[i]
+    print(upper_str)
