4febe1c6510e1ad8813a8d77464635547e43ab5d
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-08-11 21:03:18 +0300

    sum up all argumments: wrong indentation

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,8 +5,7 @@ if __name__ == "__main__":
     if (len(arg_list) == 1):
         print("0")
     else:
-        sum = 0
+        res = 0
         for i in range(1, len(arg_list)):
-            sum = sum + int(arg_list[i])
-
-    print("{}".format(sum))
+            res = res + int(arg_list[i])
+        print("{}".format(res))
