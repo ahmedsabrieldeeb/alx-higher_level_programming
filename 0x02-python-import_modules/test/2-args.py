791df1610862add6e887ed2819cfdf275272ad6b
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-08-11 20:18:53 +0300

    printing arguments: single and plural issues

--- 0x02-python-import_modules/test/2-args.py
+++ 0x02-python-import_modules/test/2-args.py
@@ -5,6 +5,9 @@ if __name__ == "__main__":
     len_arg_list = len(arg_list)
     if (len_arg_list == 1):
         print("0 arguments.")
+    elif (len_arg_list == 2):
+        print("1 argument:")
+        print("1: {}".format(arg_list[1]))
     else:
         print("{} arguments:".format(len_arg_list - 1))
         for i in range(1, len_arg_list):
