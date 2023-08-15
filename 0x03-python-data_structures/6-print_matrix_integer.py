9c6b43975312a0b03a64694a22fe7f9a95b82757
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-08-16 02:55:31 +0300

    print_matrix_integer() func: first attempt

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/bin/usr/python3
+
+def print_matrix_integer(matrix=[[]]):
+    if matrix:
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if j == len(matrix[0])-1:
+                    print("{:d} ".format(matrix[i][j]))
+                else:
+                    print("{:d} ".format(matrix[i][j]), end='')
+        return
