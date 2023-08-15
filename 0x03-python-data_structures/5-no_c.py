949e80409d1e9f29e8dc8e02be4e550ed2183228
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-08-15 23:30:15 +0300

    no_c() func: second attempt

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    if my_string:
+    if len(my_string) != 0:
         characters_to_remove = ['c', 'C']
         result_string = "".join(char for char in my_string if char not in characters_to_remove)
         return result_string
+    return ""
+    
