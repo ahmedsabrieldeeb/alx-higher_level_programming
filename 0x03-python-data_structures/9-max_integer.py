8712791a6208d96983b5e2599643ca6c9dc5a410
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-08-16 03:50:46 +0300

    max_integer() func: second attempt

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,6 @@
 def max_integer(my_list=[]):
     if (len(my_list) == 0):
         return None
-    
-    copy = my_list.copy()
-    copy.sort()
-    return copy[-1]
+
+    my_list.sort()
+    return my_list[-1]
