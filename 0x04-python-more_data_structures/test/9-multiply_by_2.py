ec48a16e1c4c4739c94de4dcc6a7152d1353c34a
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-08-16 20:13:24 +0300

    multiply_by_2() func: multiply by 2

--- 0x04-python-more_data_structures/test/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/test/9-multiply_by_2.py
@@ -2,7 +2,6 @@
 
 def multiply_by_2(a_dictionary):
     new_dict = {}
-    for key, val in a_dictionary.items():
-        if val % 2 == 0:
-            new_dict[key] = val
+    for key in a_dictionary.keys():
+        new_dict[key] = a_dictionary[key] * 2
     return new_dict
