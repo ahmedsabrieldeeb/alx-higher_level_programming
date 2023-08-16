2b946e14c2416f01c9bce3eb33f517c4f76a0412
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-08-16 04:08:32 +0300

    divisible_by_2() func: formatting issues

--- 0x03-python-data_structures/test/10-divisible_by_2.py
+++ 0x03-python-data_structures/test/10-divisible_by_2.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-
 def divisible_by_2(my_list=[]):
-    
     new_list = []
-    for element in my_list:
-        if (element % 2 == 0):
+    for i in my_list:
+        if i % 2 == 0:
             new_list.append(True)
         else:
             new_list.append(False)
