c6bdb90de34150c380e4d61dbbb40675de53223d
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-08-29 04:40:37 +0300

    bugs

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,7 +6,7 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except ValueError as err1:
-        print(f"Exception: {err1}"s, file=sys.stderr)
+        print(f"Exception: {err1}", file=sys.stderr)
         return False
     except TypeError as err2:
         print(err2, file=sys.stderr)
