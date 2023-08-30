c5e046b74eda8b6dd85a4c7a3d1a795ac643137a
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-08-30 03:28:38 +0300

    using private attribute

--- 0x06-python-classes/test/1-square.py
+++ 0x06-python-classes/test/1-square.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""A class to define a Square."""
+
+
+class Square:
+    """Defining a square."""
+
+    __size = 0
+
+    def __init__(self, size):
+        """
+        Args:
+            self: argument referring to object calling the method.
+            size: size of square
+        """
+
+        self.__size = size
