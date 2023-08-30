7d6571bc293908014814df5d3c2afd26b5107f22
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-08-30 03:36:49 +0300

    formatting issue

--- 0x06-python-classes/test/2-square.py
+++ 0x06-python-classes/test/2-square.py
@@ -13,7 +13,7 @@ class Square:
             self: argument referring to object calling the method.
             size (int): size of square
         """
-        if not isinstance(size,int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
