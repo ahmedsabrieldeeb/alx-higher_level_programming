147cf3df872846a07dc301ada8a4f0f6b88d5a1f
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-09-05 01:32:31 +0300

    init folder setup

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+
+"""
+    A module that cotains only one function
+    a function that prints your full name.
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    A function to print: My name is <first name> <last name>
+
+    Args:
+        first_name (string): the first name.
+        last_name (string):  the last name.
+        Defaults to empty string if not provided.
+
+    Returns:
+        No returns
+
+    Raises:
+        TypeError: first_name must be a string
+        TypeError: last_name must be a string
+    """
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print(f"My name is {first_name} {last_name}")
