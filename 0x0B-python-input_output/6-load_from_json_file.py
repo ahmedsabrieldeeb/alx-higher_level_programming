2acfed2680586a32b0becb2dc152890588eff680
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-09-13 01:09:57 +0300

    load_from_json_file() func: load JSON file

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""
+Module of seventh task
+"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Load JSON file
+
+    Args:
+        filename (str): the file name of JSON file
+
+    Returns:
+        corresponding python object(s)
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        json.load(f)
