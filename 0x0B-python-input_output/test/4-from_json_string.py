5facec0d26dd903eea5a0245fb0dac064fbb1fdf
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-09-13 01:00:49 +0300

    from_json_string() func: deserializing

--- 0x0B-python-input_output/test/4-from_json_string.py
+++ 0x0B-python-input_output/test/4-from_json_string.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+Module of fifth task
+"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    Represent the object in JSON
+
+    Args:
+        my_str: a string coming from JSON
+
+    Returns:
+        the original obejct
+    """
+    return (json.loads(my_str))
