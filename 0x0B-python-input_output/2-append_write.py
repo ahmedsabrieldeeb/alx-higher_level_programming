da195575b9d754716508ae33d2cb7154ace62d0c
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-09-12 23:32:48 +0300

    append_write(): append at the end

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+Module of third task
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    append a text to a file in python at the end
+
+    Args:
+        filename (str): filename or path
+        text (str): the content to be written
+
+    Returns:
+        int: number of characters written
+    """
+    with open(filename, mode="a", encoding="utf-8") as f:
+        return (f.write(text))
