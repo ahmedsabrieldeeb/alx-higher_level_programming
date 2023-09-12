b5c6453f57d1c12dc1130792fd67b01cbc2dd0d7
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-09-13 01:05:48 +0300

    save_to_json_file() func: save a JSON file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+Module of sixth task
+"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    Save JSON file
+
+    Args:
+        my_obj: object to be serialized
+        filename (str): the file name of JSON file
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(my_obj, f)
