851fdaa6b60cf09805e67ea3943426143e39115a
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-09-13 00:52:09 +0300

    to_json_string() func: serialize with json

--- 0x0B-python-input_output/test/3-to_json_string.py
+++ 0x0B-python-input_output/test/3-to_json_string.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+Module of fourth task
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Represent the object in JSON
+
+    Args:
+        myobj: a python object
+
+    Returns:
+        JSON represntation
+    """
+    return (json.dumps(my_obj))
