af3f3e81b32c8581123108ca24168f5f5755a252
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-09-12 23:19:24 +0300

    read_file(): prevent print to print newline

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -11,4 +11,4 @@ def read_file(filename=""):
         filename (str): filename or path
     """
     with open(filename, mode="r", encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end='')
