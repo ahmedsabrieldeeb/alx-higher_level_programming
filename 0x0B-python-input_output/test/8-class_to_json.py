a70f3ef196adfafd595a5d9947e775661e76986d
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-09-13 15:36:56 +0300

    class_to_json() func: return obj __dict__

--- 0x0B-python-input_output/test/8-class_to_json.py
+++ 0x0B-python-input_output/test/8-class_to_json.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+Module of ninth task
+"""
+
+
+def class_to_json(obj):
+    """
+    function that returns the dictionary description with simple data structure
+    such as (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object
+
+    Args:
+        obj: python object
+
+    Returns:
+        corresponding object __dict__ attribute
+    """
+    return (obj.__dict__)
