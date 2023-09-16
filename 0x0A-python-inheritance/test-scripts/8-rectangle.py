4844b90cd2fd728f4eafa2773544187079228c08
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-09-16 17:54:12 +0300

    Rectangle(BaseGeometry) class: inheriting example

--- 0x0A-python-inheritance/test-scripts/8-rectangle.py
+++ 0x0A-python-inheritance/test-scripts/8-rectangle.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""A module having one class implemented inherting from another"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """A class that inherits from BaseGeometry with more methods"""
+
+    def __init__(self, width, height):
+        """
+        Constructor to set rectangle's width and height
+        Without getter and setter
+
+        Args:
+
+            width (int): width of rectangle
+            height (int): height of rectangle
+        """
+
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
