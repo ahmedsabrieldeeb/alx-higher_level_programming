0e0cec38fe6bc318e4d09486165d46ce241c6b40
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-09-19 03:31:24 +0300

    testing type and value of attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,60 +6,120 @@ from models.base import Base
 
 
 class Rectangle(Base):
-	"""A Recntangle class inheriting from Base class"""
+    """A Recntangle class inheriting from Base class"""
 
-	def __init__(self, width, height, x=0, y=0, id=None):
-		"""
-		A constructor to initialize class objects
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        A constructor to initialize class objects
 
-		Args:
-			width (int): width of recatangle
-			height (int): height of rectangle
-			x (int): x-coordinate
-			y (int): y-coordinate
-		"""
-		super().__init__(id)
-		self.width = width
-		self.height = height
-		self.x = x
-		self.y = y
+        Args:
+            width (int): width of recatangle
+            height (int): height of rectangle
+            x (int): x-coordinate
+            y (int): y-coordinate
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
-	@property
-	def width(self):
-		"""Get width of Rectangle bject"""
-		return self.__width
+    @property
+    def width(self):
+        """Get width of Rectangle bject"""
+        return self.__width
 
-	@width.setter
-	def width(self, value):
-		"""Set width of Rectangle object"""
-		self.__width = value
+    @width.setter
+    def width(self, value):
+        """
+        Set width of Rectangle object
+        
+        Args:
+            value (int): value set to width
+        
+        Raises:
+            TypeError: width must be an int
+            ValueError: width must be > 0
+        """
+        if (type(value) is not int):
+            raise TypeError("width must be an int")
 
-	@property
-	def height(self):
-		"""Get height of Rectangle bject"""
-		return self.__height
+        if (value <= 0):
+            raise ValueError("width must be > 0")
 
-	@height.setter
-	def height(self, value):
-		"""Set height of Rectangle object"""
-		self.__height = value
+        self.__width = value
 
-	@property
-	def x(self):
-		"""Get x of Rectangle bject"""
-		return self.__x
+    @property
+    def height(self):
+        """Get height of Rectangle bject"""
+        return self.__height
 
-	@x.setter
-	def x(self, value):
-		"""Set x of Rectangle object"""
-		self.__x = value
+    @height.setter
+    def height(self, value):
+        """
+        Set height of Rectangle object
+        
+        Args:
+            value (int): value set to height
+        
+        Raises:
+            TypeError: height must be an int
+            ValueError: height must be > 0
+        """
+        if (type(value) is not int):
+            raise TypeError("height must be an int")
 
-	@property
-	def y(self):
-		"""Get y of Rectangle bject"""
-		return self.__y
+        if (value <= 0):
+            raise ValueError("height must be > 0")
 
-	@y.setter
-	def y(self, value):
-		"""Set y of Rectangle object"""
-		self.__y = value
+        self.__height = value
+
+    @property
+    def x(self):
+        """Get x of Rectangle bject"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """
+        Set x-coordinate of Rectangle object
+        
+        Args:
+            value (int): value set to x-coordinate
+        
+        Raises:
+            TypeError: x must be an int
+            ValueError: x must be >= 0
+        """
+        if (type(value) is not int):
+            raise TypeError("x must be an int")
+
+        if (value < 0):
+            raise ValueError("x must be >= 0")
+
+        self.__x = value
+
+    @property
+    def y(self):
+        """Get y of Rectangle bject"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """
+        Set y-coordinate of Rectangle object
+        
+        Args:
+            value (int): value set to y-coordinate
+        
+        Raises:
+            TypeError: y must be an int
+            ValueError: y must be >= 0
+        """
+        if (type(value) is not int):
+            raise TypeError("y must be an int")
+
+        if (value < 0):
+            raise ValueError("y must be >= 0")
+
+        self.__y = value
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -4,33 +4,169 @@
 
 import unittest
 from models.rectangle import Rectangle
+from models.base import Base
 
 
-class TestBase(unittest.TestCase):
+class TestRectangle(unittest.TestCase):
     """A test class to test rectangle class"""
 
-    def test_init(self):
-        """Test cases of class objects"""
-        self.rect_1 = Rectangle(10, 2)
-        self.assertEqual(self.rect_1.id, 1)
+    def setUp(self):
+        """cleaning up before each test case utilizing name mangling"""
+        Base._Base__nb_objects = 0
 
-        self.rect_2 = Rectangle(10, 2)
-        self.assertEqual(self.rect_2.id, 2)
 
-        self.rect_3 = Rectangle(10, 2, 0, 0, 15)
-        self.assertEqual(self.rect_3.id, 15)
+    def test_init_default(self):
+        """Test passing default number of args"""
+        rect = Rectangle(10, 5)
+        self.assertEqual(rect.id, 1)
+        self.assertEqual(rect.width, 10)
+        self.assertEqual(rect.height, 5)
+        self.assertEqual(rect.x, 0)
+        self.assertEqual(rect.y, 0)
 
-        self.rect_4 = Rectangle(10, 2)
-        self.assertEqual(self.rect_4.width, 10)
+    def test_init_wrong_args_number(self):
+        with self.assertRaises(TypeError):
+            Rectangle()
 
-        self.rect_5 = Rectangle(10, 2)
-        self.assertEqual(self.rect_5.height, 2)
+        with self.assertRaises(TypeError):
+            Rectangle(10)
 
-        self.rect_6 = Rectangle(10, 2, 6)
-        self.assertEqual(self.rect_6.x, 6)
+        with self.assertRaises(TypeError):
+            Rectangle(10, 5, 6, 8, 9, 10)
 
-        self.rect_7 = Rectangle(10, 2, 0, 0, 100)
-        self.assertEqual(self.rect_7.id, 100)
+    def test_rect_is_base(self):
+        rect = Rectangle(10, 10)
+        self.assertEqual(isinstance(rect, Base), True)
+
+    def test_init_of_base_behaviour(self):
+        rect = Rectangle(10, 6)
+        self.assertEqual(rect.id, 1)
+
+        rect = Rectangle(10, 6)
+        self.assertEqual(rect.id, 2)
+
+        rect = Rectangle(10, 6)
+        self.assertEqual(rect.id, 3)
+
+        rect = Rectangle(10, 6, 0, 0, 10)
+        self.assertEqual(rect.id, 10)
+
+    def test_access_private(self):
+        rect = Rectangle(10, 8)
+        with self.assertRaises(AttributeError):
+            rect.__width
+
+        with self.assertRaises(AttributeError):
+            rect.__height
+
+        with self.assertRaises(AttributeError):
+            rect.__x
+
+        with self.assertRaises(AttributeError):
+            rect.__y
+
+    def test_width_type(self):
+        with self.assertRaises(TypeError):
+            Rectangle("5", 6)
+
+        with self.assertRaises(TypeError):
+            Rectangle(5.5, 6)
+
+        with self.assertRaises(TypeError):
+            Rectangle((15, ), 6)
+
+        with self.assertRaises(TypeError):
+            Rectangle([14, ], 6)
+
+        with self.assertRaises(TypeError):
+            Rectangle({14, }, 6)
+
+        with self.assertRaises(TypeError):
+            Rectangle({"num": 5}, 6)
+
+    def test_width_value(self):
+        with self.assertRaises(ValueError):
+            Rectangle(-6, 6)
+
+        with self.assertRaises(ValueError):
+            Rectangle(0, 6)
+
+
+    def test_height_type(self):
+        with self.assertRaises(TypeError):
+            Rectangle(6, "5")
+
+        with self.assertRaises(TypeError):
+            Rectangle(6, 5.5)
+
+        with self.assertRaises(TypeError):
+            Rectangle(6, (15, ))
+
+        with self.assertRaises(TypeError):
+            Rectangle(6, [14, ])
+
+        with self.assertRaises(TypeError):
+            Rectangle(6, {14, })
+
+        with self.assertRaises(TypeError):
+            Rectangle(6, {"num": 5})
+
+    def test_height_value(self):
+        with self.assertRaises(ValueError):
+            Rectangle(6, -1)
+
+        with self.assertRaises(ValueError):
+            Rectangle(6, 0)
+
+
+
+    def test_x_type(self):
+        with self.assertRaises(TypeError):
+            Rectangle(6, 6, "5")
+
+        with self.assertRaises(TypeError):
+            Rectangle(6, 6, 5.5)
+
+        with self.assertRaises(TypeError):
+            Rectangle(6, 6, (15, ))
+
+        with self.assertRaises(TypeError):
+            Rectangle(6, 6, [14, ])
+
+        with self.assertRaises(TypeError):
+            Rectangle(6, 6, {14, })
+
+        with self.assertRaises(TypeError):
+            Rectangle(6, 6, {"num": 5})
+
+    def test_x_value(self):
+        with self.assertRaises(ValueError):
+            Rectangle(6, 6, -6)
+
+
+
+    def test_y_type(self):
+        with self.assertRaises(TypeError):
+            Rectangle(6, 6, 6, "5")
+
+        with self.assertRaises(TypeError):
+            Rectangle(6, 6, 6, 5.5)
+
+        with self.assertRaises(TypeError):
+            Rectangle(6, 6, 6, (15, ))
+
+        with self.assertRaises(TypeError):
+            Rectangle(6, 6, 6, [14, ])
+
+        with self.assertRaises(TypeError):
+            Rectangle(6, 6, 6, {14, })
+
+        with self.assertRaises(TypeError):
+            Rectangle(6, 6, 6, {"num": 5})
+
+    def test_y_value(self):
+        with self.assertRaises(ValueError):
+            Rectangle(6, 6, 0, -3)
 
 if __name__ == '__main__':
     unittest.main()
