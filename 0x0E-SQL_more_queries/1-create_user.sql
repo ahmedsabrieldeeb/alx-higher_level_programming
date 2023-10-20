c5ea7500983f780a639c182740ad5ad9501cc32d
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-10-20 20:43:05 +0300

    GRANT ALL PRIVILEGES statement

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- This script creates new user with all privilages
+-- Script shouldn't fail if that user exists
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
