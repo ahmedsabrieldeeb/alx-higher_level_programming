4ef05810af2996ff66ab8cc139684070d47c8df6
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-10-21 15:23:23 +0300

    without NULL

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,7 @@
 -- This script lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
 -- Using JOIN, we can do this
 SELECT title, genre_id
-FROM tv_shows LEFT OUTER JOIN tv_show_genres
+FROM tv_shows JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 ORDER BY title, genre_id;
 
