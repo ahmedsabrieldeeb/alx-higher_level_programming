7d51515d71c648e2e8ddc41751fea1a4d962213f
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-10-21 23:16:54 +0300

    multiple JOINS again

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,9 @@
+-- This script lists all Comedy shows in the database hbtn_0d_tvshows.
+-- With multiple JOINS and one SELECT
+SELECT title
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY title;
+
