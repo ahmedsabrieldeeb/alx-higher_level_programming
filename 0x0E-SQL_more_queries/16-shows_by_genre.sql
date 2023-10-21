78816b12533e02fde082feafe266b2e4c284f239
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-10-21 23:24:08 +0300

    double LEFT OUTER JOIN

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- This script lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
+-- Shows with no genre, put NULL
+SELECT title, name
+FROM tv_shows
+LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT OUTER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY title, name;
+
