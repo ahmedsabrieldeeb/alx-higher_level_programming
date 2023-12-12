cc0f466de0b32bda5b6c92e9d07be15691231c61
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-12-12 22:04:58 +0200

    task #11: second largest number

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const argv = process.argv.slice(2);
+
+for (let i = 0; i < argv.length; i++) {
+  argv[i] = Number(argv[i]);
+}
+
+if (argv.length <= 1) {
+  console.log(0);
+} else {
+  const largeNumber = Math.max(...argv);
+  argv.splice(argv.indexOf(largeNumber), 1);
+  console.log(Math.max(...argv));
+}
