f867c71864b033817311a7df258cea155ec62563
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-12-12 21:34:35 +0200

    task #10: NaN is not equal itself

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,10 +2,9 @@
 const num = Number(process.argv[2]);
 
 function factorial (n) {
-  if (n === 1) {
+  if (isNaN(n) || n === 1) {
     return (1);
   }
   return (n * factorial(n - 1));
 }
-
 console.log(factorial(num));
