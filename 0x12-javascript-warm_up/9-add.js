e51ce663aaac87ba228de433bb2bd58e643a330a
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-12-12 20:39:17 +0200

    task #9: basic function in js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+const firstArg = Number(process.argv[2]);
+const secondArg = Number(process.argv[3]);
+function add (a, b) {
+  return (a + b);
+}
+console.log(add(firstArg, secondArg));
