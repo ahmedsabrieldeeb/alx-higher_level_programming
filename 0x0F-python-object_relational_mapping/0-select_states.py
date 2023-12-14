1bacf88deb1d90c7b1f72cbb5858388bc4533014
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-12-14 20:23:35 +0200

    task #0: simple connection and fetching

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,1 +1,44 @@
 #!/usr/bin/python3
+"""
+    Connecting to mysql-server fetching some data from database "hbtn_0e_0_usa"
+"""
+
+import sys
+import MySQLdb
+
+
+def main():
+    """main program"""
+
+    # check the proper usage of module
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        exit()
+
+    # fetching the credentials from the input arguments
+    username, password, database = sys.argv[1:]
+
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=database)
+    cursor = db.cursor()
+
+    # write here the QUERY
+    sql_query = "SELECT * FROM states ORDER BY id"
+
+    cursor.execute(sql_query)
+    rows = cursor.fetchall()
+
+    # displaying the resuts
+    for row in rows:
+        print(row)
+
+    # closing everything before leaving
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
