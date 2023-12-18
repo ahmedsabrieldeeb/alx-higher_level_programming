3937cf62086406b805bf88dc7114fdddd6b0e5fb
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-12-18 03:31:27 +0200

    task #10: fetching a specific id of a given statename

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,1 +1,47 @@
 #!/usr/bin/python3
+"""
+    a script to fetach all data coming from a table
+    in mysql database
+"""
+
+import sys
+from model_state import Base, State
+
+from sqlalchemy.engine.url import URL
+from sqlalchemy.orm import sessionmaker
+
+from sqlalchemy import (create_engine)
+
+
+def main():
+    """ main program """
+
+    # check the proper usage of module
+    if len(sys.argv) != 5:
+        print("Usage: {} <username> <password> <database> <statename>"
+              .format(sys.argv[0]))
+        exit()
+
+    # pool_pre_ping ensures working with non-stale connections
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    # extract a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # extract all states in states table referring to its mapped class
+    state = session.query(State).filter(State.name == sys.argv[4]).first()
+
+    # printing state id fetched from our database if existed
+    if state:
+        print(state.id)
+    else:
+        print("Not found")
+
+
+if __name__ == "__main__":
+    main()
