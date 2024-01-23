7c49627dfaa85a3c5ae31b9ed6adaf2a786e8216
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-01-23 02:37:40 +0200

    Update curl command to include -L flag

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL, sends a GET request to that URL, and displays the body of the response if the status code is 200
-response=$(curl -s -o /dev/null -w "%{http_code}" "$1"); [ "$response" -eq 200 ] && curl -s "$1"
+response=$(curl -s -o /dev/null -w "%{http_code}" "$1"); [ "$response" -eq 200 ] && curl -sL "$1"
